4a3d32488ff9f1fdc0ee96f622296b54abc2aa26
Youssoufboz <bouzekriyousouf@gmail.com>
2025-12-07 00:50:35 +0100

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import path from "path";
+
 import authRouter from'./routes/auth.route.js';
 import MessageRouter from'./routes/message.route.js';
+
 const app=express();
 const PORT=process.env.PORT || 3000;
 dotenv.config();
 console.log(process.env.PORT); 
+const __dirname =path.resolve();
 
 
 
@@ -14,6 +18,12 @@ app.use("/api/auth",authRouter);
 app.use("/api/messages",MessageRouter);
 
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));}
+
+app.get("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
 
 
 
